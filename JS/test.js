98e99e3b004947f3d73af63e8dce1513c685ffb9
christiaanvlas <151550@csgaugustinus.nl>
2021-11-11 11:06:14 +0000

    hoi

--- JS/test.js
+++ JS/test.js
@@ -1,64 +1,109 @@
-class cirkel{
-  constructor(){
-  this.x= random(10,1000);
-  this.y= random(10,1000);
-  this.straal= 30;
-  this.snelheid= 20;
-  this.kleur= 'red';
-  this.G = random(2,400);
-  this.R= random(2,400);
-  this.B= random(2,400);
+var jager = {
+  x: 50,
+  y: 50,
+  zijde: 100,
+  stapGrootte: 2,
+
+  beweeg() {
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= this.stapGrootte;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += this.stapGrootte;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= this.stapGrootte;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      this.y += this.stapGrootte;
+    }
+    
+    this.x = constrain(this.x,0,canvas.width - this.zijde);
+    this.y = constrain(this.y,0,canvas.height - this.zijde);
+  },
+  
+  vlakbijRand() {
+    if (this.x < 4*this.stapGrootte || this.x > canvas.width - 4*this.stapGrootte - this.zijde || this.y < 4*this.stapGrootte || this.y > canvas.height - 4*this.stapGrootte - this.zijde) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  },
+  
+  teken() {
+    fill('dodgerblue');
+    rect(this.x,this.y,this.zijde,this.zijde);
   }
-teken(){
-  noStroke();
-  fill(this.R,this.G,this.B);
-  ellipse(this.x,this.y,this.straal);
-}
-beweeg(){
-  if (keyIsDown(LEFT_ARROW)) {
-    this.x -= this.snelheid;
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    this.x += this.snelheid;
-  }
-  if (keyIsDown(UP_ARROW)) {
-    this.y -=this.snelheid;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    this.y += this.snelheid;
-  }
-}
 }
 
-var cirkels = [];
+
+var prooi = {
+  x: 800,
+  y: 175,
+  breedte: 75,
+  hoogte: 50,
+  benGeraakt: false,
+  
+  wordJeGeraakt(vijand) {
+    if (vijand.x >= this.x - vijand.zijde && vijand.x <= this.x + this.breedte && vijand.y > this.y - vijand.zijde && vijand.y < this.y + this.hoogte) {
+      this.benGeraakt = true;
+    }
+  },  
+  
+  teken() {
+    if(this.benGeraakt) {
+      fill('white');
+    }
+    else {
+      fill('green');
+    }
+    rect(this.x,this.y,this.breedte,this.hoogte);
+  }
+}
 
 
 function setup() {
-  canvas = createCanvas(1000,1000);
-
+  canvas = createCanvas(1000,400);
   canvas.parent('processing');
-  //noLoop();
-  for(var c =0; c <3;c++){ 
-    cirkels.push(new cirkel());
-  }
-  // cirkel1 = new cirkel(200,225,80,2,'red');
-  // cirkel2 = new cirkel(200,225,80,5,'blue');
-  // cirkel3 = new cirkel(200,225,80,8,'purple');
-
-}
-
-
-function draw() { 
-   background('silver');
-
   noStroke();
+  frameRate(50);
+}
 
-  for(n=0;n<cirkels.length ;n++) { 
-    if (cirkels[n].x || cirkels[n].y > 1000){
-    cirkels[n].teken();
-    cirkels[n].beweeg();
+function draw() {
+  if (jager.vlakbijRand()) {
+    background('red');
+    prooi.benGeraakt = false;
   }
-}
+  else {
+    background('orange');
+  }
+  
+  jager.beweeg();
+  jager.teken();
  
+beweeg() {
 
-}
+  if (mouseIsPressed) {
+
+   
+
+      this.frameNummer = 2;
+
+    }
+
+    else {
+
+      this.frameNummer = 1;
+
+  function preload() {
+
+    brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+  
+    for (var b = 0;b < jos.aantalFrames;b++) {
+  
+      frame = loadImage("images/sprites/Jos100px/Jos_" + b + ".png");
+  
+      jos.animatie.push(frame);
+  
+    }
