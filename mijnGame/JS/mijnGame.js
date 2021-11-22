1367bb207483b091a17df9c728bc636c22c4cb9e
christiaanvlas <151550@csgaugustinus.nl>
2021-11-22 15:45:44 +0000

    jo

--- mijnGame/JS/mijnGame.js
+++ mijnGame/JS/mijnGame.js
@@ -1,13 +1,15 @@
 function setup(){
-    canvas = createCanvas((windowWidth, windowHeight);  
-    textAlign(CENTER,CENTER);  
-    frameRate(50);
-
+   canvas = createCanvas(windowWidth, windowHeight);
+   textAlign(CENTER,CENTER);    
+   frameRate(50);
 }
-function preload() {
-    achtergrond = loadImage("images/skyline_1.jpg");
-  }
+ 
+ function preload()  { 
+        achtergrond = loadImage("image/autoweg.jpg"); 
+ }
 
-  function draw() {
-   background(achtergrond);
-  }
+function draw() { 
+        background(achtergrond); 
+     }
+
+
